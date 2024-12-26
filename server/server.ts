75365fce5a644641831a7d1bc6258163a74213f1
sanjayr-12 <sanjayr.bit22@gmail.com>
2024-12-26 14:57:21 +0530

    first commit

---
+++ server/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log("server started");
+});
