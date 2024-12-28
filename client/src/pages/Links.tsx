3198f6b6d8f4e67ede07fbf72b3b59d1e557db65
sanjayr-12 <sanjayr.bit22@gmail.com>
2024-12-28 08:01:00 +0530

    updated

--- client/src/pages/Links.tsx
+++ client/src/pages/Links.tsx
@@ -1,11 +1,7 @@
-import { useLocation } from "react-router-dom";
 const Links = () => {
-  const location = useLocation();
-
   return (
     <div className="primary-content min-h-screen p-20">
       Links
-      {console.log(location)}
     </div>
   );
 };
